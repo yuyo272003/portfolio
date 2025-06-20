027f4ece8e8445961c0cd872aa13d614d331b9ea
julio <zS2201737@estudiantes.uv.mx>
2025-06-19 23:03:02 -0600

    netlify deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
-const repoName = "portfolio"
-
 const nextConfig = {
   output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  assetPrefix:
+      process.env.NODE_ENV === "production" ? "/portfolio/" : "",
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 }
-
 export default nextConfig
-
